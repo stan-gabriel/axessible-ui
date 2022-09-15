3e7bf08ef9eb78a55841cd1c04b0c9666f87118b
gabriel-stan <stan.gabriel88@gmail.com>
2022-09-15 18:27:56 +0300

    add config for tslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
-        <div>TEST</div>
+      <header className="App-header" />
+      <div>TEST</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
