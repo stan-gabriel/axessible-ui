b47fc5137dcafe6c3920917cfc69f82b178e9abe
gabriel-stan <stan.gabriel88@gmail.com>
2022-09-16 17:57:16 +0300

    implement design

--- src/Pages/InvoiceInfo/InvoiceInfo.tsx
+++ src/Pages/InvoiceInfo/InvoiceInfo.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { Paper, TextField } from '@mui/material'
+import { InputAdornment, Paper, TextField } from '@mui/material'
+import MenuItem from '@mui/material/MenuItem'
+import Stack from '@mui/material/Stack/Stack'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import Tooltip from '@mui/material/Tooltip'
 
 interface InvoiceInfoProps {}
 
@@ -26,28 +30,34 @@ const InvoiceInfo: FC<InvoiceInfoProps> = () => {
   return (
     <Box sx={{ mt: '21px', pl: '130px', pr: '130px' }}>
       <Typography variant="h4" sx={{ m: '21px' }}>
-        Invoice Info
+        Invoice details
       </Typography>
+
       <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', p: '10px 26px' }}>
-        <Typography variant="h6" sx={{ m: '21px' }}>
-          General Info
-        </Typography>
+        <Typography variant="h6">General Info</Typography>
 
-        <Box>
+        <Box
+          sx={{
+            '& .MuiTextField-root': { m: 1 },
+            display: 'flex',
+          }}
+        >
           <TextField
             required
             id="filled-required"
-            label="Shipping Date"
+            label="Invoice number"
+            type="number"
             defaultValue=""
             variant="filled"
-          />
-
-          <TextField
-            required
-            id="filled-required"
-            label="Shipping Arrival Customs"
-            defaultValue=""
-            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
           />
 
           <TextField
@@ -58,14 +68,209 @@ const InvoiceInfo: FC<InvoiceInfoProps> = () => {
             value={invoiceTypeCode}
             onChange={handleChange}
             variant="filled"
-            helperText="Please select your currency"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
           >
             {invoiceTypeCodes.map((option) => (
-              <option key={option.value} value={option.value}>
+              <MenuItem key={option.value} value={option.value}>
                 {option.label}
-              </option>
+              </MenuItem>
             ))}
           </TextField>
+
+          <TextField
+            required
+            id="filled-required"
+            label="Invoice Date"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="INCO term"
+            type="number"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="Currency"
+            defaultValue="USD"
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+
+        <Typography variant="h6">Exporter details</Typography>
+
+        <Box
+          sx={{
+            '& .MuiTextField-root': { m: 1 },
+          }}
+        >
+          <TextField
+            required
+            id="filled-required"
+            label="Cargo ID"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="Duns number"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="Country Code"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="VAT ID"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+
+        <Typography variant="h6">Importer details</Typography>
+
+        <Box
+          sx={{
+            '& .MuiTextField-root': { m: 1 },
+          }}
+        >
+          <TextField
+            required
+            id="filled-required"
+            label="Name"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="Country Code"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="VAT ID"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Some info">
+                    <InfoOutlinedIcon fontSize="small" />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
         </Box>
       </Paper>
     </Box>
