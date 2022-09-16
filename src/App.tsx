9ee0c86454fba39d88d07836869ef37494055127
gabriel-stan <stan.gabriel88@gmail.com>
2022-09-16 14:11:14 +0300

    add NavBar, avatar and logo to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
-      <div>TEST</div>
+      <NavBar />
     </div>
   )
 }
