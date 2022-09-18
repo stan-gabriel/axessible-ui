3a7fb348ec17c2d220db6acc3b9a03335a0a99c9
gabriel-stan <stan.gabriel88@gmail.com>
2022-09-19 02:10:48 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
-import InvoiceInfo from './Pages/InvoiceInfo/InvoiceInfo'
+import InvoiceInfo from './pages/InvoiceInfo/InvoiceInfo'
 import { GlobalContextProvider } from './context/GlobalContext'
 import Loader from './components/loader'
 
--- src/api/InvoiceInfoApi.ts
+++ src/api/InvoiceInfoApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { IInvoiceInfo } from '../Pages/InvoiceInfo/invoiceInfo.types'
+import { IInvoiceInfo } from '../pages/InvoiceInfo/invoiceInfo.types'
 
 const api = axios.create({
   baseURL: '',
